85cdb24d07dbbdbdad85fe530b40801a8196c74b
Matej Genci <matej.genci@gmail.com>
2026-06-06 17:25:37 +0100

    Procrastinate by faffing with main.cpp

--- include/types.h
+++ include/types.h
@@ -6,3 +6,9 @@
 
 template<typename T>
 using OfxResult = std::expected<T, OfxStatus>;
+
+#define OFX_RETURN_IF_ERROR(expr)   \
+    do {                            \
+        auto __res = (expr);        \
+        if (!__res) { return __res; } \
+    } while (0)
--- main.cpp
+++ main.cpp
@@ -20,30 +20,32 @@
 using fn_ofxGetNumberOfPlugins = int (*)();
 using fn_ofxGetPlugin = OfxPlugin* (*)(int);
 
-std::vector<OfxPlugin*> GetPlugins(const char *libraryPath);
-void RenderImageWithOfx(const char *path, image::Image &srcImg);
-void ShowImage(image::Image &img);
+std::vector<OfxPlugin*> GetPlugins(const char* libraryPath);
+OfxResult<void> RenderImageWithOfx(const char* path, image::Image& srcImg);
+void ShowImage(image::Image& img);
 
-std::expected<void, OfxStatus> PluginMain(
-    const OfxPlugin* plugin, const char *action, const char *msg,
-    const void *handle = nullptr, OfxPropertySetHandle inArgs = nullptr, OfxPropertySetHandle outArgs = nullptr
-);
+OfxResult<void> PluginMain(
+    const OfxPlugin* plugin, const char* action, const char* msg,
+    const void* handle = nullptr, OfxPropertySetHandle inArgs = nullptr, OfxPropertySetHandle outArgs = nullptr);
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
+    if (argc != 2) {
         std::cerr << "App requires an argument with a path to an OFX plugin DLL\n";
         return 1;
     }
 
     auto img = image::Image::load("./starry_night.jpg");
-    RenderImageWithOfx(argv[1], img);
+    if (!RenderImageWithOfx(argv[1], img)) {
+        std::cerr << "Failed to render image\n";
+        return 1;
+    }
+
     ShowImage(img);
     return 0;
 }
 
-void RenderImageWithOfx(const char *path, image::Image &srcImg)
+OfxResult<void> RenderImageWithOfx(const char* path, image::Image& srcImg)
 {
     Host ofxHost;
     ImageEffect effect;
@@ -57,15 +59,13 @@ void RenderImageWithOfx(const char *path, image::Image &srcImg)
     const auto plugin = plugins[0];
     plugin->setHost(ofxHost.OfxHandle());
 
-    if (!PluginMain(plugin, kOfxActionLoad,     "Loading plugin")) { return; }
-    if (!PluginMain(plugin, kOfxActionDescribe, "Describing plugin", effect.OfxHandle())) { return; }
+    OFX_RETURN_IF_ERROR(PluginMain(plugin, kOfxActionLoad, "Loading plugin"));
+    OFX_RETURN_IF_ERROR(PluginMain(plugin, kOfxActionDescribe, "Describing plugin", effect.OfxHandle()));
 
     // Describe in general context.
     inArgs.SetString(kOfxImageEffectPropContext, 0, kOfxImageEffectContextGeneral);
-    if (!PluginMain(plugin, kOfxImageEffectActionDescribeInContext,
-                    "ImageEffect describing plugin in context", effect.OfxHandle(), inArgs.OfxHandle())) {
-        return;
-    }
+    OFX_RETURN_IF_ERROR(PluginMain(plugin, kOfxImageEffectActionDescribeInContext,
+        "ImageEffect describing plugin in context", effect.OfxHandle(), inArgs.OfxHandle()));
     std::print("Plugin description in general context:\n");
     effect.DebugPrint();
     inArgs.Clear();
@@ -79,26 +79,27 @@ void RenderImageWithOfx(const char *path, image::Image &srcImg)
     // nb. Modify the image in place. Probably not always valid but oh hey.
 
     inArgs.SetDouble(kOfxPropTime, 0, 0.0);
-    inArgs.SetN(kOfxImageEffectPropRenderWindow, {0, 0, (int)srcImg.width() / 2, (int)srcImg.height()});
-    if (!PluginMain(plugin, kOfxImageEffectActionRender, "ImageEffect render",
-                    effect.OfxHandle(), inArgs.OfxHandle())) {
-        return;
-    }
-
+    inArgs.SetN(kOfxImageEffectPropRenderWindow, { 0, 0, (int)srcImg.width() / 2, (int)srcImg.height() });
+    OFX_RETURN_IF_ERROR(PluginMain(plugin, kOfxImageEffectActionRender,
+        "ImageEffect render", effect.OfxHandle(), inArgs.OfxHandle()));
 
     // Cleanup.
-    if (!PluginMain(plugin, kOfxActionUnload,   "Unloading plugin")) { return; }
+    OFX_RETURN_IF_ERROR(PluginMain(plugin, kOfxActionUnload, "Unloading plugin"));
+
+    return {};
 }
 
-std::vector<OfxPlugin*> GetPlugins(const char *libraryPath)
+std::vector<OfxPlugin*> GetPlugins(const char* libraryPath)
 {
     // We're leaking this handle.
 #if defined(WIN32) || defined(WIN64)
-    const HMODULE ofxDll = LoadLibraryA(libraryPath); assert(ofxDll != nullptr);
+    const HMODULE ofxDll = LoadLibraryA(libraryPath);
+    assert(ofxDll != nullptr);
     const FARPROC getNumPluginsPtr = GetProcAddress(ofxDll, "OfxGetNumberOfPlugins");
     const FARPROC getPluginPtr = GetProcAddress(ofxDll, "OfxGetPlugin");
 #elif defined(__linux__)
-    void* ofxDll = dlopen(libraryPath, RTLD_LAZY); assert(ofxDll != nullptr);
+    void* ofxDll = dlopen(libraryPath, RTLD_LAZY);
+    assert(ofxDll != nullptr);
     void* getNumPluginsPtr = dlsym(ofxDll, "OfxGetNumberOfPlugins");
     void* getPluginPtr = dlsym(ofxDll, "OfxGetPlugin");
 #endif
@@ -119,23 +120,22 @@ std::vector<OfxPlugin*> GetPlugins(const char *libraryPath)
     return result;
 }
 
-std::expected<void, OfxStatus> PluginMain(
-    const OfxPlugin* plugin, const char *action, const char *msg,
-    const void *handle, OfxPropertySetHandle inArgs, OfxPropertySetHandle outArgs
-)
+OfxResult<void> PluginMain(
+    const OfxPlugin* plugin, const char* action, const char* msg,
+    const void* handle, OfxPropertySetHandle inArgs, OfxPropertySetHandle outArgs)
 {
     std::print("{}...\n", msg);
     OfxStatus status = plugin->mainEntry(action, handle, inArgs, outArgs);
     if (status != kOfxStatOK) {
         std::print("Failed with {}\n", status);
-        return std::unexpected{status};
+        return std::unexpected { status };
     }
 
     std::print("OK\n\n");
     return {};
 }
 
-void ShowImage(image::Image &img)
+void ShowImage(image::Image& img)
 {
     std::print("Width: {} x Height {}\n", img.width(), img.height());
     img.show_blocking();
